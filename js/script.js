fca901ae470cd785f5c0d3812e85035f88abe92c
antonius1980 <antonius1980@gmail.com>
2025-07-24 18:35:39 +0300

    Formatted files

--- js/script.js
+++ js/script.js
@@ -1,25 +1,25 @@
-const btn = document.getElementById('profile_photo_remove');
-const inputFile = document.getElementById('profile_pic');
-const previewImage = document.querySelector('.profile-photo-row__image');
+const btn = document.getElementById("profile_photo_remove");
+const inputFile = document.getElementById("profile_pic");
+const previewImage = document.querySelector(".profile-photo-row__image");
 
 // При выборе файла — обновляем preview
-inputFile.addEventListener('change', () => {
-  const file = inputFile.files[0];
+inputFile.addEventListener("change", () => {
+	const file = inputFile.files[0];
 
-  if (file) {
-    const reader = new FileReader();
-    reader.onload = function (e) {
-      previewImage.src = e.target.result;
-    };
-    reader.readAsDataURL(file);
-  }
+	if (file) {
+		const reader = new FileReader();
+		reader.onload = function (e) {
+			previewImage.src = e.target.result;
+		};
+		reader.readAsDataURL(file);
+	}
 });
 
 // При клике на Remove — сбрасываем input и картинку
-btn.addEventListener('click', () => {
-  inputFile.value = '';
-  previewImage.src = './assets/img/profile_photo.png'; // путь по умолчанию
+btn.addEventListener("click", () => {
+	inputFile.value = "";
+	previewImage.src = "./assets/img/profile_photo.png"; // путь по умолчанию
 });
 
-  const input = document.getElementById('profile_phone');
-    Inputmask("+38 (099) 999-9999").mask(input);
+const input = document.getElementById("profile_phone");
+Inputmask("+38 (099) 999-9999").mask(input);
